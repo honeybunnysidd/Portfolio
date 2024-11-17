1086f3c1cc1bcc207c4b33cefccf481b8098f742
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2024-11-17 19:04:17 +0530

    add file

---
+++ app.js
@@ -0,0 +1,7 @@
+let docTitle = document.title;
+    window.addEventListener("blur", () => {
+        document.title = "Come Back 🙋🏻‍♀️";
+    });
+    window.addEventListener("focus", () => {
+        document.title = docTitle;
+    })
